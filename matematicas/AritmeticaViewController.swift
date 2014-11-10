97d6b7dbfefb8b22333bbe88df1f0196355f4718
Luis Flores <me@luisflores.mx>
2014-11-09 19:02:15 -0600

    Add menu view controller

---
+++ matematicas/AritmeticaViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AritmeticaViewController.swift
+//  matematicas
+//
+//  Created by Luis Flores on 11/9/14.
+//  Copyright (c) 2014 Luis Flores. All rights reserved.
+//
+
+import UIKit
+
+class AritmeticaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Aritmética"
+    }
+}
--- matematicas/GeometriaViewController.swift
+++ matematicas/GeometriaViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GeometriaViewController.swift
 //  matematicas
 //
 //  Created by Luis Flores on 11/9/14.
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GeometriaViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Geometría"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
